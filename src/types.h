e2d1e29c2bd3af519f7371e01fbbabf69fa77843
Daniel Lovasko <lovasko@freebsd.org>
2015-07-26 16:04:19 +0200

    mapping types

---
+++ src/types.h
@@ -0,0 +1,39 @@
+#include <stdint.h>
+
+struct ps_proc;
+
+struct ps_proc_list_entry {
+	struct ps_proc* le_next;
+};
+
+struct ps_timeval {
+	intmax_t tv_sec;
+	intmax_t tv_usec;
+};
+
+struct ps_pstats {
+	struct ps_timeval p_start;
+};
+
+struct ps_ucred {
+	uintmax_t cr_uid;
+};
+
+struct ps_rusage {
+	struct ps_timeval ru_utime;
+	struct ps_timeval ru_stime;
+};
+
+struct ps_proc {
+	intmax_t p_pid;	
+	intmax_t p_numthreads;
+	struct ps_pstats* p_stats;
+	struct ps_ucred* p_ucred;
+	struct ps_rusage p_ru;
+	struct ps_proc_list_entry p_list;
+};
+
+struct ps_proclist {
+	struct ps_proc* lh_first;
+};
+
