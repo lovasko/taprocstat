5a8d949b2ba0f5bc3204c998243b16e67fd8cca5
Daniel Lovasko <lovasko@freebsd.org>
2015-08-02 01:30:10 +0200

    ppid fix in mapping types

--- src/types.h
+++ src/types.h
@@ -26,13 +26,13 @@ struct ps_rusage {
 
 struct ps_proc {
 	intmax_t p_pid;
-	intmax_t p_ppid;
 	intmax_t p_numthreads;
 	intmax_t p_comm[20];
 	struct ps_pstats* p_stats;
 	struct ps_ucred* p_ucred;
 	struct ps_rusage p_ru;
 	struct ps_proc_list_entry p_list;
+	struct ps_proc* p_pptr;
 };
 
 struct ps_proclist {
